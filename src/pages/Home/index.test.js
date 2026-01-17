a91c457655d059c906eb30b723b55289cde58428
Killian <killiangayez@gmail.com>
2026-01-17 09:44:40 +0100

    Ajout de test d'intégration et de test unitaires

--- src/pages/Home/index.test.js
+++ src/pages/Home/index.test.js
@@ -1,5 +1,34 @@
 import { fireEvent, render, screen } from "@testing-library/react";
+import { api, DataProvider } from "../../contexts/DataContext";
 import Home from "./index";
+
+const mockData = {
+  events: [
+    {
+      id: 1,
+      type: "conférence",
+      date: "2022-08-29T20:28:45.744Z",
+      title: "Event A",
+      cover: "/images/a.png",
+      description: "desc",
+      nb_guesses: 1,
+      periode: "x",
+      prestations: [],
+    },
+    {
+      id: 2,
+      type: "forum",
+      date: "2022-01-01T00:00:00.000Z",
+      title: "Event B",
+      cover: "/images/b.png",
+      description: "desc",
+      nb_guesses: 1,
+      periode: "x",
+      prestations: [],
+    },
+  ],
+  focus: [],
+};
 
 describe("When Form is created", () => {
   it("a list of fields card is displayed", async () => {
@@ -36,16 +65,30 @@ describe("When Form is created", () => {
 
 
 describe("When a page is created", () => {
-  it("a list of events is displayed", () => {
-    // to implement
+  it("a list of events is displayed", async () => {
+    api.loadData = jest.fn().mockReturnValue(mockData);
+    render(
+      <DataProvider>
+        <Home />
+      </DataProvider>
+    );
+    await screen.findByTestId("events-list");
   })
-  it("a list a people is displayed", () => {
-    // to implement
+  it("a list a people is displayed", async () => {
+    render(<Home />);
+    await screen.findByTestId("people-list");
   })
-  it("a footer is displayed", () => {
-    // to implement
+  it("a footer is displayed", async () => {
+    render(<Home />);
+    await screen.findByTestId("footer");
   })
-  it("an event card, with the last event, is displayed", () => {
-    // to implement
+  it("an event card, with the last event, is displayed", async () => {
+    api.loadData = jest.fn().mockReturnValue(mockData);
+    render(
+      <DataProvider>
+        <Home />
+      </DataProvider>
+    );
+    await screen.findByTestId("last-prestation");
   })
 });
